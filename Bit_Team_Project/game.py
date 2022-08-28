0b3a2f3f3d2654433872264527141963cd49016f
BuildTools <unconfigured@null.spigotmc.org>
2022-08-28 20:31:32 +0900

    h

--- Bit_Team_Project/game.py
+++ Bit_Team_Project/game.py
@@ -1,2 +1,16 @@
 import pygame
-from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 200))
+screen.fill(WHITE)
+pygame.display.update
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+pygame.quit()
