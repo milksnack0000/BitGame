da3ae13774f24e6595634137ab51f5b2655014c1
hyerin03 <112103908+hyerin03@users.noreply.github.com>
2022-10-25 23:05:19 +0900

    Add files via upload

--- Bit_Team_Project/game.py
+++ Bit_Team_Project/game.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 
+import math #적 플레이어 추적
+
 pygame.init() #초기화
 
 #화면 설정
@@ -9,10 +11,11 @@ WHITE = (255, 255, 255)
 screen.fill(WHITE)
 pygame.display.set_caption("BIT_GAME")
 
+clock = pygame.time.Clock()
+
 """surf = pygame.Surface((400, 600))
 surf.fill(WHITE)
 S_rect = surf.get_rect()""" #맵 제한 테스트용
-
 
 #플레이어, Sprite 클래스를 바탕으로 만듦
 class Player(pygame.sprite.Sprite):
@@ -26,9 +29,9 @@ class Player(pygame.sprite.Sprite):
 
 player = Player()
 
-
 #적, Sprite 클래스를 바탕으로 만듦
 class Enemy(pygame.sprite.Sprite):
+
     def __init__(self):
         super(Enemy, self).__init__() #여기까지 무시해도 됨
 
@@ -39,15 +42,39 @@ class Enemy(pygame.sprite.Sprite):
 
 enemy = Enemy()
 
+#적 플레이어 추적
+distance = 0
+
+px= player.rect.x
+py= player.rect.y
+
+dx, dy = 0,0
 
 running = True
 while running:
+
+    clock.tick(60)
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
 
-    #screen.blit(surf, S_rect)
+    
+    #적 플레이어 추적
 
+    #플레이어와 적 사이의 direction vector (dx, dy) 찾기
+    dx, dy = px - enemy.rect.x, py - enemy.rect.y
+    dist = math.hypot(dx, dy)
+    dx, dy = dx / dist, dy / dist  # Normalize.
+    distance = (math.hypot(enemy.rect.x  - px, enemy.rect.y - py) )
+    distance = int(distance)
+    
+    # 적이 normalized vector을 따라 플레이어를 향해 이동(속도 조절 가능)
+    enemy.rect.x += dx *1
+    enemy.rect.y += dy *1
+
+    
     #플레이어 화면에 보이기
     pygame.draw.rect(screen, (0, 0, 255), player.rect)
     screen.blit(player.surf, player.rect)
@@ -56,6 +83,9 @@ while running:
     pygame.draw.rect(screen, WHITE, enemy.rect)
     screen.blit(enemy.surf, enemy.rect)
     
+    #screen.blit(surf, S_rect)
+    
     pygame.display.update()
+    
 
 pygame.quit()
