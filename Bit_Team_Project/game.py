3439a773e2050935784230f80c47ea87687df036
Heeyoons <114038193+Heeyoons@users.noreply.github.com>
2022-11-12 13:09:48 +0900

    Update game.py

--- Bit_Team_Project/game.py
+++ Bit_Team_Project/game.py
@@ -1,17 +1,16 @@
 import pygame
 from pygame.locals import *
+import random # 적 랜덤 생성
+import math #적 플레이어 추적
 
 pygame.init() #초기화
 
 #화면 설정
-screen = pygame.display.set_mode((300, 540))
+screen_width = 900
+screen_height = 1080
+screen = pygame.display.set_mode((screen_width, screen_height))
 WHITE = (255, 255, 255)
-screen.fill(WHITE)
 pygame.display.set_caption("BIT_GAME")
-
-"""surf = pygame.Surface((400, 600))
-surf.fill(WHITE)
-S_rect = surf.get_rect()""" #맵 제한 테스트용
 
 
 #플레이어, Sprite 클래스를 바탕으로 만듦
@@ -22,7 +21,7 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.image.load("player.png").convert_alpha() #이미지 불러오기
         self.surf.set_colorkey((255, 255, 255), RLEACCEL) #투명한 부분 색 선택
         self.rect = self.surf.get_rect()
-        self.rect.center = 150, 270
+        self.rect.center = screen_width/2, screen_height/2
 
 player = Player()
 
@@ -35,10 +34,20 @@ class Enemy(pygame.sprite.Sprite):
         self.surf = pygame.image.load("enemy.png").convert_alpha() #이미지 불러오기
         self.surf.set_colorkey((255, 255, 255), RLEACCEL) #투명한 부분 색 선택
         self.rect = self.surf.get_rect()
-        self.rect.center = 50, 270
+        
+#적 추가하는 이벤트
+second = 250
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, second) #현재 0.25초마다 실행
 
-enemy = Enemy()
+#스프라이트 그룹
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
+#적 플레이어 추적
+
+dx, dy = 0,0
 
 running = True
 while running:
@@ -46,31 +55,78 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #screen.blit(surf, S_rect)
+        elif event.type == ADDENEMY:
+            enemy = Enemy()
+            #적 랜덤 생성
+            coordinate = 0, 0
+            x = random.randint(-320, screen_width + 320)
+            y = random.randint(-320, screen_height + 320)
+            if -120 < x < screen_width + 120 and -120 < y < screen_height + 120:
+                a = x + screen_width, x - screen_width
+                b = y + screen_height, y - screen_height
+                coordinate = random.choice(a), random.choice(b)
 
-    #플레이어 화면에 보이기
-    pygame.draw.rect(screen, (0, 0, 255), player.rect)
-    screen.blit(player.surf, player.rect)
+            enemy.rect.center = coordinate
+            enemies.add(enemy)
+            all_sprites.add(enemy)
 
-    #적 화면에 보이기
-    pygame.draw.rect(screen, WHITE, enemy.rect)
-    screen.blit(enemy.surf, enemy.rect)
+    #플레이어 위치 업데이트용
+    px = player.rect.x
+    py = player.rect.y
+    for enemy in enemies:
+        #플레이어와 적 사이의 direction vector (dx, dy) 찾기
+        dx, dy = px - enemy.rect.x, py - enemy.rect.y
+        dist = math.hypot(dx, dy)
+        if dist != 0:
+            dx, dy = dx / dist, dy / dist  # Normalize.
     
+        # 적이 normalized vector을 따라 플레이어를 향해 이동(속도 조절 가능)
+        enemy.rect.x += dx * 1
+        enemy.rect.y += dy * 1
+
+    screen.fill(WHITE)
+
+    #적과 플레이어 화면에 표시
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+    
+    enemies.update()
     pygame.display.update()
 
 pygame.quit()
 
- #점수
-    point = 0
-    font_point = pygame.font.SysFont(None,30)
-    text_point = font_pont.render(str(point)),True,(255,0,0)
-    background.blit(text_point,(10,10))
+import pygame
+import sys
+from pygame.locals import QUIT
 
-    #적을 물리칠때마다 포인트 점수 +5
-    point += 5
+pygame.init()
+pygame.display.set_caption('BIT_GAME')
+SURFACE = pygame.display.set_mode((1000,600))
+FPSCLOCK = pygame.time.Clock()
+Big_font = pygame.font.SysFont(None, 150)
+Small_font = pygame.font.SysFont(None, 40)
 
-    #초기화 및 디스플레이 설정
 
-    pygame.init()
-    background = pygame.display.set_mode((480,360))
-    pygame.display. set_caption("UniverseGame")
+def main():
+    Score = 0
+    Miss = 0
+    message_Title = Big_font.render("BIT GAME", True, (255, 0 , 255))  # 게임제목 적기
+    message_Score = Small_font.render("Score: {}".format(Score), True, (255, 255, 255))  # 스코어
+    message_Miss = Small_font.render("Miss_Point: {}".format(Miss), True, (255, 255, 255))  # 놓친장애물수
+    message_caution = Small_font.render("Game instructioin ", True, (255, 255, 0))
+    while True:
+        SURFACE.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        SURFACE.blit(message_Title, (250,250))  # 화면상에 제목 표시
+
+        SURFACE.blit(message_caution, (650, 400))  # 화면상에 주의사항 표시
+
+     
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+if __name__ == '__main__':
+    main()
