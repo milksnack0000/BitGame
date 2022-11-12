f9155ef4bd028eca79fb8397323c1dc5574c3617
hyerin03 <112103908+hyerin03@users.noreply.github.com>
2022-11-12 11:45:40 +0900

    Update game.py

--- Bit_Team_Project/game.py
+++ Bit_Team_Project/game.py
@@ -1,21 +1,18 @@
 import pygame
 from pygame.locals import *
-
+import random # 적 랜덤 생성
 import math #적 플레이어 추적
+import time #살아남은 
 
 pygame.init() #초기화
 
 #화면 설정
-screen = pygame.display.set_mode((300, 540))
+screen_width = 900
+screen_height = 1080
+screen = pygame.display.set_mode((screen_width, screen_height))
 WHITE = (255, 255, 255)
-screen.fill(WHITE)
 pygame.display.set_caption("BIT_GAME")
 
-clock = pygame.time.Clock()
-
-"""surf = pygame.Surface((400, 600))
-surf.fill(WHITE)
-S_rect = surf.get_rect()""" #맵 제한 테스트용
 
 #플레이어, Sprite 클래스를 바탕으로 만듦
 class Player(pygame.sprite.Sprite):
@@ -25,68 +22,88 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.image.load("player.png").convert_alpha() #이미지 불러오기
         self.surf.set_colorkey((255, 255, 255), RLEACCEL) #투명한 부분 색 선택
         self.rect = self.surf.get_rect()
-        self.rect.center = 150, 270
+        self.rect.center = screen_width/2, screen_height/2
 
 player = Player()
 
+
 #적, Sprite 클래스를 바탕으로 만듦
 class Enemy(pygame.sprite.Sprite):
-
     def __init__(self):
         super(Enemy, self).__init__() #여기까지 무시해도 됨
 
         self.surf = pygame.image.load("enemy.png").convert_alpha() #이미지 불러오기
         self.surf.set_colorkey((255, 255, 255), RLEACCEL) #투명한 부분 색 선택
         self.rect = self.surf.get_rect()
-        self.rect.center = 50, 270
+        
+#적 추가하는 이벤트
+second = 250
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, second) #현재 0.25초마다 실행
 
-enemy = Enemy()
+#스프라이트 그룹
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 #적 플레이어 추적
-distance = 0
-
-px= player.rect.center[0]
-py= player.rect.center[1]
 
 dx, dy = 0,0
 
 running = True
+startTime = time.time()
+
 while running:
-
-    clock.tick(60)
-
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
+            print(a) #살아남은 시간 출력
             running = False
 
-    
-    #적 플레이어 추적
+        elif event.type == ADDENEMY:
+            enemy = Enemy()
+            #적 랜덤 생성
+            coordinate = 0, 0
+            x = random.randint(-320, screen_width + 320)
+            y = random.randint(-320, screen_height + 320)
+            if -120 < x < screen_width + 120 and -120 < y < screen_height + 120:
+                a = x + screen_width, x - screen_width
+                b = y + screen_height, y - screen_height
+                coordinate = random.choice(a), random.choice(b)
 
-    #플레이어와 적 사이의 direction vector (dx, dy) 찾기
-    dx, dy = px - enemy.rect.center[0], py - enemy.rect.center[1]
-    dist = math.hypot(dx, dy)
-    if dist != 0:
-        dx, dy = dx / dist, dy / dist  # Normalize.
-    distance = (math.hypot(enemy.rect.center[0]  - px, enemy.rect.center[1] - py) )
-    distance = int(distance)
-    
-    # 적이 normalized vector을 따라 플레이어를 향해 이동(속도 조절 가능)
-    enemy.rect.x += dx *1
-    enemy.rect.y += dy *1
+            enemy.rect.center = coordinate
+            enemies.add(enemy)
+            all_sprites.add(enemy)
 
+    #플레이어 위치 업데이트용
+    px = player.rect.x
+    py = player.rect.y
+    for enemy in enemies:
+        #플레이어와 적 사이의 direction vector (dx, dy) 찾기
+        dx, dy = px - enemy.rect.x, py - enemy.rect.y
+        dist = math.hypot(dx, dy)
+        if dist != 0:
+            dx, dy = dx / dist, dy / dist  # Normalize.
     
-    #플레이어 화면에 보이기
-    pygame.draw.rect(screen, (0, 0, 255), player.rect)
-    screen.blit(player.surf, player.rect)
+        # 적이 normalized vector을 따라 플레이어를 향해 이동(속도 조절 가능)
+        enemy.rect.x += dx * 1
+        enemy.rect.y += dy * 1
 
-    #적 화면에 보이기
-    pygame.draw.rect(screen, WHITE, enemy.rect)
-    screen.blit(enemy.surf, enemy.rect)
+    screen.fill(WHITE)
+
+    #적과 플레이어 화면에 표시
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
     
-    #screen.blit(surf, S_rect)
+    enemies.update()
     
+    #살아남은 시간
+    font = pygame.font.SysFont(None, 32)
+    a = str(int(time.time() - startTime))
+    counting_text = font.render(a, 1, (0,0,0))
+    counting_rect = counting_text.get_rect(center = screen.get_rect().center)
+    screen.blit(counting_text, counting_rect)
+
     pygame.display.update()
-    
 
 pygame.quit()
