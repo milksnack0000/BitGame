03561f5cdfe359d0d2816cb00af6712ff325c086
milksnack0000 <milksnack0000@gmail.com>
2022-11-16 12:44:30 +0900

    체력. 데미지는 아직. 전체화면. 화살표로 체력 증가와 감소. fps 추가.

--- Bit_Team_Project/game.py
+++ Bit_Team_Project/game.py
@@ -6,9 +6,9 @@ import math #적 플레이어 추적
 pygame.init() #초기화
 
 #화면 설정
-screen_width = 900
-screen_height = 1080
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((0,0), FULLSCREEN)
+screen_width = int(screen.get_width())
+screen_height = int(screen.get_height())
 WHITE = (255, 255, 255)
 pygame.display.set_caption("BIT_GAME")
 
@@ -22,6 +22,46 @@ class Player(pygame.sprite.Sprite):
         self.surf.set_colorkey((255, 255, 255), RLEACCEL) #투명한 부분 색 선택
         self.rect = self.surf.get_rect()
         self.rect.center = screen_width/2, screen_height/2
+        #체력
+        self.current_health = 200
+        self.target_health = 1000
+        self.max_health = 1000
+        self.health_bar_length = 40
+        self.health_ratio = self.max_health / self.health_bar_length
+        self.health_change_speed = 2
+    def get_damage(self,amount):
+        if self.target_health > 0:
+            self.target_health -= amount
+        if self.target_health < 0:
+            self.target_health = 0
+
+    def get_health(self,amount):
+        if self.target_health < self.max_health:
+            self.target_health += amount
+        if self.target_health > self.max_health:
+            self.target_health = self.max_health
+
+    def advanced_health(self):
+        transition_width = 0
+        transition_color = (255,0,0)
+
+        if self.current_health < self.target_health:
+            self.current_health += self.health_change_speed
+            transition_width = int((self.target_health - self.current_health) / self.health_ratio)
+            transition_color = (0,255,0)
+
+        if self.current_health > self.target_health:
+            self.current_health -= self.health_change_speed 
+            transition_width = int((self.target_health - self.current_health) / self.health_ratio)
+            transition_color = (255,255,0)
+
+        health_bar_width = int(self.current_health / self.health_ratio)
+        health_bar = pygame.Rect(screen_width/2 -20,screen_height/2 +10,health_bar_width,6)
+        transition_bar = pygame.Rect(health_bar.right,screen_height/2 +10,transition_width,6)
+		
+        pygame.draw.rect(screen,(255,0,0),health_bar)
+        pygame.draw.rect(screen,transition_color,transition_bar)	
+        pygame.draw.rect(screen,(0, 0, 0),(screen_width/2 -20,screen_height/2 +10,self.health_bar_length,6),1)
 
 player = Player()
 
@@ -34,7 +74,14 @@ class Enemy(pygame.sprite.Sprite):
         self.surf = pygame.image.load("enemy.png").convert_alpha() #이미지 불러오기
         self.surf.set_colorkey((255, 255, 255), RLEACCEL) #투명한 부분 색 선택
         self.rect = self.surf.get_rect()
-        
+
+def colllided():
+    #여기에 충돌 시 hp 깎는 걸 만들자.
+    #if pygame.sprite.spritecollideany(player, enemies):
+        #player.get_damage(1)
+    pass
+
+clock = pygame.time.Clock()
 #적 추가하는 이벤트
 second = 250
 ADDENEMY = pygame.USEREVENT + 1
@@ -46,33 +93,42 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
 #적 플레이어 추적
-
 dx, dy = 0,0
 
+#실행
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        #적 랜덤 생성
         elif event.type == ADDENEMY:
             enemy = Enemy()
-            #적 랜덤 생성
             coordinate = 0, 0
             x = random.randint(-320, screen_width + 320)
             y = random.randint(-320, screen_height + 320)
+
             if -120 < x < screen_width + 120 and -120 < y < screen_height + 120:
                 a = x + screen_width, x - screen_width
                 b = y + screen_height, y - screen_height
                 coordinate = random.choice(a), random.choice(b)
+            else: coordinate = x, y
 
             enemy.rect.center = coordinate
             enemies.add(enemy)
             all_sprites.add(enemy)
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.get_health(200)
+            if event.key == pygame.K_DOWN:
+                player.get_damage(200)
 
     #플레이어 위치 업데이트용
     px = player.rect.x
     py = player.rect.y
+
     for enemy in enemies:
         #플레이어와 적 사이의 direction vector (dx, dy) 찾기
         dx, dy = px - enemy.rect.x, py - enemy.rect.y
@@ -84,6 +140,7 @@ while running:
         enemy.rect.x += dx * 1
         enemy.rect.y += dy * 1
 
+    #화면표시
     screen.fill(WHITE)
 
     #적과 플레이어 화면에 표시
@@ -91,6 +148,8 @@ while running:
         screen.blit(entity.surf, entity.rect)
     
     enemies.update()
-    pygame.display.update()
+    player.advanced_health()
 
+    pygame.display.update()
+    clock.tick(720)
 pygame.quit()
